116453be12a3641bd9d9f6cf1426bdb8b5bb6308
AdamRauff <adam.rauff@ucdenver.edu>
2017-06-23 13:54:30 -0600

    First Commit

---
+++ bleExp/detect.py
@@ -0,0 +1,19 @@
+# Adam Rauff
+# 6/19/2017
+# Smart Door Project
+
+import bluetooth
+import time
+
+while True:
+	print("Checking " + time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()))
+	result = bluetooth.lookup_name('',timeout=5)
+
+	if (result != None):
+		print('User Present')
+	else:
+		print('Out of range')
+
+	time.sleep(7) 
+
+
